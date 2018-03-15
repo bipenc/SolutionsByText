e2e84f77cda57393f37a733e5fe7a6e40ee5e6e8
sijanonly <sijanonly@gmail.com>
2018-03-15 17:09:03 +0545

    Adds solutionsbytext package

---
+++ solutions_by_text/utils.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: leapfrong
+# @Date:   2018-03-15 14:14:21
+# @Last Modified by:   leapfrong
+# @Last Modified time: 2018-03-15 14:15:50
+
+
+def build_payload():
+    pass
